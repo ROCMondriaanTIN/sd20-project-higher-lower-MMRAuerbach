d87853cd98b6810672a7b36298a15932af440247
MMR Auerbach <m.auerbach@rocmondriaan.nl>
2021-02-03 15:03:29 +0100

    update

--- Card.cs
+++ Card.cs
@@ -1,11 +1,170 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace ProjectHigherLower
 {
-    // Vervang de class Card met de Card van card-assignment opdracht zodra die af is.
-    class Card
+    public class Card
     {
+        // Attributes
+        private string suit;
+        private string colour;
+        private string value;
+        private int points;
+        private static string[] allowedSuits = { "♡", "♢", "♧", "♤" };
+        private static string[] allowedValues = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+
+        /**
+         * Constructor
+         * @params string, string, int
+         */
+        public Card(string value, string suit, int points)
+        {
+            this.SetValue(value);
+            this.SetSuit(suit);
+            this.points = points;
+        }
+
+        /**
+         * PrintCard - shows a card in a console.write
+         * @params - 
+         * @return void
+         */
+        public void PrintCard()
+        {
+            //Store the old colours in a var
+            ConsoleColor oldBackground = Console.BackgroundColor;
+            ConsoleColor oldForeground = Console.ForegroundColor;
+
+            //Change the console colour to new colour
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+
+            //When the card colour is red, then change foreground to red
+            if (this.GetColour() == "red")
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+
+            //Write the suit en value
+            Console.Write(this.suit+this.value);
+
+            //Change the console colours back to the old values (as stored in begin of method)
+            Console.ForegroundColor = oldForeground;
+            Console.BackgroundColor = oldBackground;
+        }
+
+        /**
+         * CheckValue - Check a value against the allowed values for the card
+         * @params string
+         * @return bool
+         */
+        public bool CheckValue(string value)
+        {
+            bool isAllowed = false;
+            for (int i = 0; i < allowedValues.Length; i++)
+            {
+                if (value == allowedValues[i])
+                {
+                    isAllowed = true;
+                }
+            }
+
+            return isAllowed;
+        }
+
+        /**
+         * This method returns the points of this card.
+         * @params - 
+         * @return int
+         */
+        public int GetPoints()
+        {
+            return points;
+        }
+
+        /**
+         * SetSuit - Sets the Suit of the card AND the colour that represents that suit.
+         * @params string
+         * @return void
+         */
+        public void SetSuit(string suit)
+        {
+            if (suit == "♡" || suit == "♢" || suit == "♧" || suit == "♤")
+            {
+                this.suit = suit;
+                this.colour = GetColour(suit);
+            }
+            else
+            {
+                throw new Exception("Could not find the specified suit.");
+            }
+        }
+
+        /**
+         * SetValue -   This method checks the value and if the value is allowed it sets the value.
+         *              This method method uses the CheckValue method to check if the value is correct.
+         * @params string
+         * @return void
+         */
+        public void SetValue(string value)
+        {
+            if (CheckValue(value))
+            {
+                this.value = value;
+            }
+            else
+            {
+                throw new Exception("This value of a card is not allowed");
+            }
+        }
+
+        /**
+         * GetColour - Get the colour of the card
+         * @params - 
+         * @return string
+         */
+        public string GetColour()
+        {
+            return this.colour;
+        }
+
+        /**
+         * GetColour - Get the colour according to the given Suit
+         * @params string
+         * @return string
+         */
+        public string GetColour(string suit)
+        {
+            if (suit == "♡" || suit == "♢")
+            {
+                return "red";
+            }
+            else if (suit == "♧" || suit == "♤")
+            {
+                return "black";
+            }
+            else
+            {
+                throw new Exception("Could not find the specified suit.");
+            }
+        }
+
+        /**
+         * GetAllowedCardValues - Returns all the allowed Card Values
+         * @params - 
+         * @return string[]
+         */
+        public static string[] GetAllowedCardValues()
+        {
+            return allowedValues;
+        }
+
+        /**
+         * GetAllowedCardSuits - Returns all the allowed Card Suits
+         * @params - 
+         * @return string[]
+         */
+        public static string[] GetAllowedCardSuits()
+        {
+            return allowedSuits;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,12 +1,185 @@
 ﻿using System;
+using System.Threading;
+using System.Collections.Generic;
 
 namespace ProjectHigherLower
 {
     class Program
     {
+        private static int userPoints = 10;
+        private static Card firstComputerCard;
+
+        /**
+         * Main - start of the game
+         * @params string[]
+         * @return void
+         */
         static void Main(string[] args)
         {
-            Console.WriteLine("Project Higher Lower - The card guessing game.");
+            Console.WriteLine("-=> Project Higher Lower - The card guessing game. <=-");
+            Console.Beep();
+            Console.Write("What is your name? ");
+            string playerName = Console.ReadLine();
+            Console.WriteLine("Welcome to this game " + playerName);
+            List<Card> deck = GenerateDeck();
+            firstComputerCard = GetRandomCard(deck);
+
+            //Create while loop, as long as player plays game then loop ;-) 
+            bool playGame = true;
+            do
+            {
+                //Start with playing a round (always)
+                PlayRound(deck);
+
+                //Check if the player can continue or wants to continue
+                Console.Write("Currentpoints = " + userPoints);
+
+                if (userPoints <= 0)
+                {
+                    playGame = false;
+                    Console.WriteLine("You have no points left, you lost.");
+                }
+                else
+                {
+                    Console.Write(" Do you want to continue Y/N? ");
+                    string userAnswer = Console.ReadLine().ToUpper();
+                    while (checkAnswer(userAnswer, new string[] { "Y", "N" }))
+                    {
+                        Console.Write("That is not a correct answer, please try again. Do you want to continue Y/N? ");
+                        userAnswer = Console.ReadLine().ToUpper();//Make sure the answer is uppercase
+                    }
+
+                    if (userAnswer == "N")
+                        playGame = false;
+
+                }
+
+            } while (playGame);
+
+            //End the game
+            Console.WriteLine("Thank you for playing HiLo, you ended with " + userPoints + " points");
+            ShowAllCards(deck);
+            Console.WriteLine("Press any key to quit...");
+            Console.ReadLine();
+        }
+
+        /**
+         * ShowAllCards - Show all the cards in the deck
+         * @params List<Card>
+         * @return void
+         */
+        static private void ShowAllCards(List<Card> deck)
+        {
+            for (int i = 0; i < deck.Count; i++)
+            {
+                deck[i].PrintCard();
+            }
+            Console.WriteLine();
+        }
+
+        /**
+         * PlayRound
+         * @params List<Card>
+         * @return void
+         */
+        static private void PlayRound(List<Card>deck)
+        {            
+            Card secondComputerCard = GetRandomCard(deck);
+            int difference = firstComputerCard.GetPoints() - secondComputerCard.GetPoints();
+            Console.Write("The computer has chosen a card, ");
+            firstComputerCard.PrintCard();
+
+            Console.Write(" is the next card higher (H) or lower (L)? ");
+
+            //Make sure the answer is uppercase
+            string userAnswer = Console.ReadLine().ToUpper();
+
+            while (checkAnswer(userAnswer, new string[] { "H", "L"}))
+            {
+                Console.WriteLine("That is not a correct answer, please try again");
+                Console.WriteLine(" is the next card higher (H) or lower (L)? ");
+                secondComputerCard.PrintCard();
+
+                userAnswer = Console.ReadLine().ToUpper();//Make sure the answer is uppercase
+            }
+
+            bool guessed = ((difference > 0 && userAnswer == "L") || (difference < 0 && userAnswer == "H"));
+
+            Console.Write("The second card is ");
+            secondComputerCard.PrintCard();
+
+            if (guessed)
+            {
+                Console.WriteLine(" your answer is correct!");
+                userPoints++;
+            }
+            else
+            {
+                Console.WriteLine(" your answer is wrong!");
+                userPoints--;
+            }
+
+            firstComputerCard = secondComputerCard;
+        }
+
+        /**
+         * GetRandomCard - select a random card from a deck
+         * @params List<Card>
+         * @return Card
+         */
+        static private Card GetRandomCard(List<Card> deck)
+        {
+            Random rnd = new Random();
+            int cardIndex = rnd.Next(deck.Count);
+
+            return deck[cardIndex];
+        }
+
+        /**
+         * GenerateDeck
+         * @params none
+         * @return List<Card>
+         */
+        static private List<Card> GenerateDeck()
+        {
+            string[] suits = Card.GetAllowedCardSuits();
+            string[] cardValue = Card.GetAllowedCardValues(); 
+
+            List<Card> deck = new List<Card>();
+
+            for (int i = 0; i < cardValue.Length; i++)
+            {
+                string useValue = cardValue[i];
+
+                for (int j = 0; j < suits.Length; j++)
+                {
+                    string useSuit = suits[j];
+                    int usePoints = i + 2;
+                    deck.Add(new Card(useValue, useSuit, usePoints));
+                }
+            }
+
+            return deck;
+        }
+
+        /**
+         * checkAnswer - check answer from user against correct options
+         * @params string, string[]
+         * @return bool
+         */
+        public static bool checkAnswer(string answer, string[] options)
+        {
+            bool returnBoolean = true;
+
+            for (int i = 0; i < options.Length; i++)
+            {
+                if (answer == options[i])
+                {
+                    returnBoolean = false;
+                }
+            }
+
+            return returnBoolean;
         }
     }
 }
