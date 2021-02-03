5d64ee1151f44a926dfee431afa45321d30c49ee
MMR Auerbach <m.auerbach@rocmondriaan.nl>
2021-02-03 17:44:10 +0100

    Multi player

---
+++ Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ProjectHigherLower
+{
+    public class Player
+    {
+        public string Name { get; set; } //Makes method Player.Name and Player.Name = ".."
+        public int Points { get; set; } //Since this is a getter-setter the variable is PascalCase
+        public string Choice { get; set; } //What was the players choice
+
+        public Player(string name)
+        {
+            Name = name;
+            Points = 0;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,8 +6,9 @@ namespace ProjectHigherLower
 {
     class Program
     {
-        private static int userPoints = 10;
         private static Card firstComputerCard;
+        private static List<Card> deck;
+        private static List<Player> players;
 
         /**
          * Main - start of the game
@@ -17,11 +18,22 @@ namespace ProjectHigherLower
         static void Main(string[] args)
         {
             Console.WriteLine("-=> Project Higher Lower - The card guessing game. <=-");
-            Console.Beep();
-            Console.Write("What is your name? ");
-            string playerName = Console.ReadLine();
-            Console.WriteLine("Welcome to this game " + playerName);
-            List<Card> deck = GenerateDeck();
+            Console.WriteLine("How many players will be playing? (1-8)");
+            string noOfPlayers = Console.ReadLine();
+            while (CheckAnswer(noOfPlayers, new string[] { "1", "2", "3", "4", "5", "6", "7", "8" }))
+            {
+                Console.WriteLine("That is not a correct number, how many players will be playing? (1-8)");
+                noOfPlayers = Console.ReadLine();
+            }
+
+
+            int numberOfPlayers;
+            Int32.TryParse(noOfPlayers, out numberOfPlayers);
+
+            AskPlayersName(numberOfPlayers);
+
+            Console.WriteLine("Welcome to this game, we start of with " + players.Count + " players");
+            GenerateDeck();
             firstComputerCard = GetRandomCard(deck);
 
             //Create while loop, as long as player plays game then loop ;-) 
@@ -32,32 +44,22 @@ namespace ProjectHigherLower
                 PlayRound(deck);
 
                 //Check if the player can continue or wants to continue
-                Console.Write("Currentpoints = " + userPoints);
+                Console.Write(" Do you want to continue Y/N? ");
 
-                if (userPoints <= 0)
+                string userAnswer = Console.ReadLine().ToUpper();
+                while (CheckAnswer(userAnswer, new string[] { "Y", "N" }))
                 {
+                    Console.Write("That is not a correct answer, please try again. Do you want to continue Y/N? ");
+                    userAnswer = Console.ReadLine().ToUpper();//Make sure the answer is uppercase
+                }
+
+                if (userAnswer == "N")
                     playGame = false;
-                    Console.WriteLine("You have no points left, you lost.");
-                }
-                else
-                {
-                    Console.Write(" Do you want to continue Y/N? ");
-                    string userAnswer = Console.ReadLine().ToUpper();
-                    while (checkAnswer(userAnswer, new string[] { "Y", "N" }))
-                    {
-                        Console.Write("That is not a correct answer, please try again. Do you want to continue Y/N? ");
-                        userAnswer = Console.ReadLine().ToUpper();//Make sure the answer is uppercase
-                    }
-
-                    if (userAnswer == "N")
-                        playGame = false;
-
-                }
 
             } while (playGame);
 
             //End the game
-            Console.WriteLine("Thank you for playing HiLo, you ended with " + userPoints + " points");
+            Console.WriteLine("Thank you for playing HiLo.");
             ShowAllCards(deck);
             Console.WriteLine("Press any key to quit...");
             Console.ReadLine();
@@ -86,37 +88,56 @@ namespace ProjectHigherLower
         {            
             Card secondComputerCard = GetRandomCard(deck);
             int difference = firstComputerCard.GetPoints() - secondComputerCard.GetPoints();
+            string winningString = "HIGHER";
+            if (difference > 0)
+                winningString = "LOWER";
+
             Console.Write("The computer has chosen a card, ");
             firstComputerCard.PrintCard();
 
-            Console.Write(" is the next card higher (H) or lower (L)? ");
+            Console.WriteLine(" every player can now choose");
 
-            //Make sure the answer is uppercase
-            string userAnswer = Console.ReadLine().ToUpper();
-
-            while (checkAnswer(userAnswer, new string[] { "H", "L"}))
+            for (int i = 0; i < players.Count; i++)
             {
-                Console.WriteLine("That is not a correct answer, please try again");
-                Console.WriteLine(" is the next card higher (H) or lower (L)? ");
-                secondComputerCard.PrintCard();
+                Player player = players[i];
+                Console.Write(player.Name + " is the next card higher (H) or lower (L)? ");
 
-                userAnswer = Console.ReadLine().ToUpper();//Make sure the answer is uppercase
+                string userAnswer = Console.ReadLine().ToUpper();
+
+                while (CheckAnswer(userAnswer, new string[] { "H", "L" }))
+                {
+                    Console.WriteLine("That is not a correct answer, please try again");
+                    Console.WriteLine(player.Name + " is the next card higher (H) or lower (L)?");
+
+                    userAnswer = Console.ReadLine().ToUpper();//Make sure the answer is uppercase
+                }
+
+                player.Choice = userAnswer;
+
             }
-
-            bool guessed = ((difference > 0 && userAnswer == "L") || (difference < 0 && userAnswer == "H"));
 
             Console.Write("The second card is ");
             secondComputerCard.PrintCard();
+            Console.WriteLine(" that means the correct answer is: " + winningString);
 
-            if (guessed)
+            for (int i = 0; i < players.Count; i++)
             {
-                Console.WriteLine(" your answer is correct!");
-                userPoints++;
-            }
-            else
-            {
-                Console.WriteLine(" your answer is wrong!");
-                userPoints--;
+                Player player = players[i];
+                bool guessed = ((difference > 0 && player.Choice == "L") || (difference < 0 && player.Choice == "H"));
+                string message = "right";
+
+                if (guessed)
+                {
+                    player.Points++;
+                    
+                }
+                else
+                {
+                    player.Points--;
+                    message = "wrong";
+                }
+
+                Console.WriteLine(player.Name + " your answer is " + message + "! You now have " + player.Points + " points.");
             }
 
             firstComputerCard = secondComputerCard;
@@ -140,12 +161,11 @@ namespace ProjectHigherLower
          * @params none
          * @return List<Card>
          */
-        static private List<Card> GenerateDeck()
+        static private void GenerateDeck()
         {
             string[] suits = Card.GetAllowedCardSuits();
-            string[] cardValue = Card.GetAllowedCardValues(); 
-
-            List<Card> deck = new List<Card>();
+            string[] cardValue = Card.GetAllowedCardValues();
+            deck = new List<Card>();
 
             for (int i = 0; i < cardValue.Length; i++)
             {
@@ -158,16 +178,14 @@ namespace ProjectHigherLower
                     deck.Add(new Card(useValue, useSuit, usePoints));
                 }
             }
-
-            return deck;
         }
 
         /**
-         * checkAnswer - check answer from user against correct options
+         * CheckAnswer - check answer from user against correct options
          * @params string, string[]
          * @return bool
          */
-        public static bool checkAnswer(string answer, string[] options)
+        public static bool CheckAnswer(string answer, string[] options)
         {
             bool returnBoolean = true;
 
@@ -180,6 +198,18 @@ namespace ProjectHigherLower
             }
 
             return returnBoolean;
+        }
+
+        static private void AskPlayersName(int numberOfPlayers)
+        {
+            players = new List<Player>();
+            for (int i = 0; i < numberOfPlayers; i++)
+            {
+                Console.WriteLine("Player " + (i + 1) + " what is your name?");
+                string useName = Console.ReadLine();
+                Player newPlayer = new Player(useName);
+                players.Add(newPlayer);
+            }
         }
     }
 }
